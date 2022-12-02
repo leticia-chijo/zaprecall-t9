08ba7765ff5da974f79fef0c34a32b35b03a5c39
leticia-chijo <leticia.chijo@gmail.com>
2022-12-02 15:46:30 -0300

    Bonus: tela inicial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useState } from "react"
 import DeckScreen from "./components/DeckScreen"
+import WelcomeScreen from "./components/WelcomeScreen"
+
 
 export default function App() {
+  const [welcome, setWelcome] = useState(true)
+
+  function goToDeckScreen() {
+    setWelcome(false)
+  }
+
   return (
     <>
-      <DeckScreen />
+      {welcome ? <WelcomeScreen goToDeckScreen={goToDeckScreen}/> : <DeckScreen />}
     </>
   )
 }
